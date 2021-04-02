c20ea0dd715ddd54f6d97f381a29fa313092d6f3
Yeison Garzón T <toxiicsa@hotmail.com>
2021-04-02 01:51:34 -0500

    ADD: global css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './App.css'
 import { BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from '../src/containers/Home/Home';
-import Price from "../src/containers/Price/Price";
+import Home from './containers/HomeContainer/HomeContainer';
+import PriceContainer from "../src/containers/PriceContainer/PriceContainer";
 import Resources from "../src/containers/Resources/Resources";
 import Contact from "../src/containers/Contact/Contact";
 import Error from "../src/containers/Error/Error";
@@ -12,12 +13,13 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path="/home" component={Home}></Route>
-        <Route exact path="/price" component={Price}></Route>
+        <Route exact path="/price" component={PriceContainer}></Route>
         <Route exact path="/resources" component={Resources}></Route>
         <Route exact path="/contact" component={Contact}></Route>
         <Route exact path="/">
           <Redirect to="/home"></Redirect>
         </Route>
+
         <Route path="*" component={Error}></Route>
       </Switch>
     </BrowserRouter>
