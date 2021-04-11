9c06162d60f79623573e62a9846d960bcfd32bb2
Yeison Garzon T <toxiicsa@hotmail.com>
2021-04-11 01:29:09 -0500

    TEST: form

--- src/containers/PriceContainer/PriceContainer.js
+++ src/containers/PriceContainer/PriceContainer.js
@@ -1,35 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import PricePage from "../../pages/PricePage/PricePage";
 
 const PriceContainer = () => {
-  const brandName = "Dev | Tools";
-  const navList = [
+
+ /*  const [formQuestions, setFormQuestions] = useState([
+    {
+      id: 1,
+      name: "Estado del proyecto",
+        checkList: [{
+          id: 1,
+          check: "Idea",
+        },
+        {
+          id: 2,
+          check: "Desarrollo"
+        },
+        {
+          id: 3,
+          check: "Producción"
+        }] 
+    },
     {
       id: 2,
-      title: "Recursos",
-      route: "resources",
-    },
-    {
-      id: 3,
-      title: "Contacto",
-      route: "contact",
-    },
-  ];
-  const reactDocsUrl = "https://es.reactjs.org/";
-  const githubProfileUrl = "https://github.com/devyeison";
-  const [currentYear, setYear] = useState("2021");
+      name: "Idea",
+       checkList: ["No tengo una idea", "Tengo una idea", "Tengo una idea y un modelo de negocio"]
+    }
+  ]); */
 
-  useEffect(() => {
-    setYear(new Date().getFullYear());
-  }, [currentYear]);
-  
+  const contentHeader = "Formulario";
+  const headingText = "Cotización";
+  const contentInfo = "Este formulario está hecho con la intención de dar una cotización aproximada del valor real del proyecto, por favor elija las opciones que más se ajustan a su proyecto"
   return (
     <PricePage
-      brandName={brandName}
-      navList={navList}
-      currentYear={currentYear}
-      reactDocsUrl={reactDocsUrl}
-      githubProfileUrl={githubProfileUrl}
+      headingText={headingText}
+      contentHeader={contentHeader}
+    /*   questions={formQuestions} */
+      contentText={contentInfo}
     />
   );
 };
