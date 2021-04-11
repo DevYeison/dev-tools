77b6a7504368391b85237aa0f9bb7a409468b024
Yeison Garzon T <toxiicsa@hotmail.com>
2021-04-11 01:27:45 -0500

    FIX: remove footer and header render from it

--- src/containers/HomeContainer/HomeContainer.js
+++ src/containers/HomeContainer/HomeContainer.js
@@ -4,8 +4,8 @@ import ResourceImg from "../../../src/assets/images/resources.png";
 import ContactImg from "../../../src/assets/images/contact.png";
 import HomePage from "../../pages/HomePage/HomePage";
 
-const Home = () => {
-  const brandName = "Dev | Tools";
+const HomeContainer = () => {
+  
   const backgroundImageText =
     "Herramientas gratuitas para cualquier desarrollador";
   const contentHeaderPhilosophy = "Filosofía";
@@ -17,22 +17,13 @@ const Home = () => {
     "El conocimiento es el arma más poderosa para cambiar el mundo y así como increíbles personas me ayudaron a mejorar, considero que puedo aportar un granito de arena al desarrollo de Software.";
   const author = "Yeison Garzón Toro.";
   const profession = "Javascript Developer";
-  const reactDocsUrl = "https://es.reactjs.org/";
-  const githubProfileUrl = "https://github.com/devyeison";
-
-  const [currentYear, setYear] = useState("2021");
   const [topicList, setTopicList] = useState([]);
-
-  useEffect(() => {
-    setYear(new Date().getFullYear());
-  }, [currentYear]);
 
   useEffect(() => {
     setTopicList([
       {
         id: 1,
         title: "Cotización",
-        route: "price",
         img: PriceImg,
         description:
           "Herramienta para elaborar una estimación (aproximada) del precio de un producto de Software, ya sea aplicación móvil, web, escritorio, plataforma etc.",
@@ -40,7 +31,6 @@ const Home = () => {
       {
         id: 2,
         title: "Recursos",
-        route: "resources",
         img: ResourceImg,
         description:
           "Recursos útiles para desarrolladores, tales como documentación de lenguajes de programación y/o frameworks simplificada, explicación de temas dificiles de comprender, herramientas y diseño para desarrollos ágiles, etc.",
@@ -48,7 +38,6 @@ const Home = () => {
       {
         id: 3,
         title: "Contacto",
-        route: "contact",
         img: ContactImg,
         description:
           "Información de contacto personal y equipo de desarrollo en caso de que busques crear un producto y no cuentes con el personal capacitado.",
@@ -57,7 +46,6 @@ const Home = () => {
   }, []);
   return (
     <HomePage
-      brandName={brandName}
       topicList={topicList}
       backgroundImageText={backgroundImageText}
       contentHeaderPhilosophy={contentHeaderPhilosophy}
@@ -67,11 +55,8 @@ const Home = () => {
       quoteText={quoteText}
       author={author}
       profession={profession}
-      currentYear={currentYear}
-      reactDocsUrl={reactDocsUrl}
-      githubProfileUrl={githubProfileUrl}
     />
   );
 };
 
-export default Home;
+export default HomeContainer;
