ce579d82d068846d3dbb0179bc375783e72c4f9e
Yeison Garzon T <toxiicsa@hotmail.com>
2021-04-11 01:12:06 -0500

    ADD: content header with test

--- src/components/ContentHeader/ContentHeader.js
+++ src/components/ContentHeader/ContentHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './ContentHeader.css';
 
 const ContentHeader = ({ contentHeaderText }) => (
-    <h2 id="contentHeader">{contentHeaderText}</h2>
+    <h2 data-test-id="headerText" id="contentHeader">{contentHeaderText}</h2>
 );
 
 export default ContentHeader;
