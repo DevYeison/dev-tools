bc7e75d912b702aafc8bb8160f9644fef6700f38
Yeison Garzon T <toxiicsa@hotmail.com>
2021-04-11 01:13:35 -0500

    ADD: header js and css update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,26 +3,28 @@ import "./Header.css";
 import { Link } from "react-router-dom";
 
 const Header = ({ brandName, topicList }) => {
+
+  const listItems = topicList.map((button) => (
+    <Link
+      className="p-2 link-element"
+      to={button.route}
+      key={button.id}
+    >
+      {button.title}
+    </Link>
+  ));
+
   return (
-    <header className="d-flex bd-highlight">
-      <Link
-        id="home-link"
-        to="/home"
-        className="p-2 flex-grow-1 bd-highlight heading"
-      >
-        <span>{brandName}</span>
+    <header className="d-flex">
+      <div id="brand-container">
+      <Link className="px-2 link-element" to="/home">
+        <span id="brand-name">{brandName}</span>
       </Link>
+      </div>
       <>
-        {topicList.map((button) => (
-          <Link
-            className="p-2 bd-highlight link-element"
-            to={button.route}
-            key={button.id}
-          >
-            {button.title}
-          </Link>
-        ))}
+        {listItems}
       </>
+
     </header>
   );
 };
