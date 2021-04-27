c143688956018f1e546573d448877ad1b3f4b263
Yeison Garzon T <toxiicsa@hotmail.com>
2021-04-26 22:50:02 -0500

    FIX: change ubication

--- src/containers/Contact/Contact.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Contact = () => "Contact";
-
-export default Contact;
--- src/containers/Resources/Resources.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Resources = () => "Resources";
-
-export default Resources;
